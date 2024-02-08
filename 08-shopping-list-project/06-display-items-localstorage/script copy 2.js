32bf30f75f5a949ff9efb9f9ea58605cbae780d7
SigridM <sigridm@selfhelpsoftware.com>
2024-02-08 15:50:21 -0500

    Created diplay-items-localstorage lesson

---
+++ 08-shopping-list-project/06-display-items-localstorage/script copy 2.js
@@ -0,0 +1,127 @@
+const itemForm = document.getElementById('item-form');
+const itemInput = document.getElementById('item-input');
+const itemList = document.getElementById('item-list');
+const clearBtn = document.getElementById('clear');
+const itemFilter = document.getElementById('filter');
+const defaultItemDisplayStyle = Array.from(
+  document.styleSheets[1].cssRules
+).find((e) => e.selectorText == '.items li').style.display;
+
+function addItem(e) {
+  e.preventDefault();
+
+  const newItem = itemInput.value;
+
+  // Validate Input
+  if (newItem === '') {
+    alert('Please add an item');
+    return;
+  }
+
+  // Create list item
+  const li = document.createElement('li');
+  li.appendChild(document.createTextNode(newItem));
+
+  const button = createButton('remove-item btn-link text-red');
+  li.appendChild(button);
+
+  // add the list item to the unordered list
+  itemList.appendChild(li);
+  checkUI();
+
+  itemInput.value = '';
+}
+
+function createButton(classes) {
+  const button = document.createElement('button');
+  button.className = classes;
+  const icon = createIcon('fa-solid fa-xmark');
+  button.appendChild(icon);
+  return button;
+}
+
+function createIcon(classes) {
+  const icon = document.createElement('i');
+  icon.className = classes;
+  return icon;
+}
+
+function removeItem(e) {
+  // console.log(e.target.parentElement);
+  if (e.target.parentElement.classList.contains('remove-item')) {
+    // console.log(e.target.parentElement.classList);
+    // console.log(e.target.parentElement.parentElement);
+    if (confirm('Are you sure?')) {
+      e.target.parentElement.parentElement.remove();
+    }
+    checkUI();
+  }
+}
+
+function clearItems() {
+  while (itemList.firstChild) {
+    itemList.removeChild(itemList.firstChild);
+  }
+  checkUI();
+}
+
+function filterItems() {
+  // console.log('Hello world'.includes('world'), typeof 'Hello world');
+
+  const filterValue = itemFilter.value.toLowerCase();
+  // console.log('itemList: ', itemList.children);
+  const itemArray = Array.from(itemList.children);
+  // console.log('filterValue: ', filterValue, typeof filterValue);
+  // console.log('itemArray: ', itemArray);
+  // const matchingItems = itemArray.filter(
+  //   (eachItem) =>
+  //     // console.log(eachItem.innerText, typeof eachItem.innerText);
+  //     eachItem.innerText.includes(filterValue)
+  //   // false;
+  // );
+  // console.log('matchingItems: ', matchingItems);
+  for (eachItem of itemArray) {
+    if (eachItem.innerText.toLowerCase().includes(filterValue)) {
+      eachItem.style.display = defaultItemDisplayStyle;
+    } else {
+      eachItem.style.display = 'none';
+    }
+  }
+}
+
+function filterItemsTraversy(e) {
+  const items = itemList.querySelectorAll('li');
+  const text = e.target.value.toLowerCase();
+
+  items.forEach((item) => {
+    const itemName = item.firstChild.textContent.toLowerCase();
+    if (itemName.indexOf(text) != -1) {
+      // indexOf() returns -1 if not found
+      item.style.display = 'flex';
+    } else {
+      item.style.display = 'none';
+    }
+  });
+}
+
+function checkUI() {
+  const items = itemList.querySelectorAll('li');
+  if (items.length === 0) {
+    clearBtn.style.display = 'none';
+    itemFilter.style.display = 'none';
+  } else {
+    clearBtn.style.display = 'block';
+    itemFilter.style.display = 'block';
+  }
+}
+// Event Listeners
+itemForm.addEventListener('submit', addItem);
+itemList.addEventListener('click', removeItem);
+clearBtn.addEventListener('click', clearItems);
+itemFilter.addEventListener('input', filterItems);
+
+checkUI();
+localStorage.setItem('name', 'Brad');
+localStorage.getItem('name');
+localStorage.removeItem('name');
+localStorage.clear();

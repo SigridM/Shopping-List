b30852f8f2f5efd26371e13688be70609d15a389
SigridM <sigridm@selfhelpsoftware.com>
2024-02-09 16:30:59 -0500

    Added items to local storage

--- 08-shopping-list-project/06-display-items-localstorage/script.js
+++ 08-shopping-list-project/06-display-items-localstorage/script.js
@@ -7,7 +7,7 @@ const defaultItemDisplayStyle = Array.from(
   document.styleSheets[1].cssRules
 ).find((e) => e.selectorText == '.items li').style.display;
 
-function addItem(e) {
+function onAddItemSubmit(e) {
   e.preventDefault();
 
   const newItem = itemInput.value;
@@ -18,18 +18,38 @@ function addItem(e) {
     return;
   }
 
+  addItemToDOM(newItem);
+  addItemToStorage(newItem);
+  checkUI();
+
+  itemInput.value = '';
+}
+function addItemToDOM(item) {
   // Create list item
   const li = document.createElement('li');
-  li.appendChild(document.createTextNode(newItem));
+  li.appendChild(document.createTextNode(item));
 
   const button = createButton('remove-item btn-link text-red');
   li.appendChild(button);
 
   // add the list item to the unordered list
   itemList.appendChild(li);
-  checkUI();
+}
 
-  itemInput.value = '';
+function addItemToStorage(item) {
+  let itemsFromStorage;
+
+  if (localStorage.getItem('items') === null) {
+    itemsFromStorage = [];
+  } else {
+    itemsFromStorage = JSON.parse(localStorage.getItem('items'));
+  }
+
+  // Add new item to array
+  itemsFromStorage.push(item);
+
+  // Convert to JSON string and set to local storage
+  localStorage.setItem('items', JSON.stringify(itemsFromStorage));
 }
 
 function createButton(classes) {
@@ -104,7 +124,7 @@ function checkUI() {
   }
 }
 // Event Listeners
-itemForm.addEventListener('submit', addItem);
+itemForm.addEventListener('submit', onAddItemSubmit);
 itemList.addEventListener('click', removeItem);
 clearBtn.addEventListener('click', clearItems);
 itemFilter.addEventListener('input', filterItems);

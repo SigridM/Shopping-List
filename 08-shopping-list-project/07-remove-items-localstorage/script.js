d3774107e22878dd5aaa791b47096b90cca653cf
SigridM <sigridm@selfhelpsoftware.com>
2024-02-10 17:41:55 -0500

    Added Show and Modify Quantities

--- 08-shopping-list-project/07-remove-items-localstorage/script.js
+++ 08-shopping-list-project/07-remove-items-localstorage/script.js
@@ -7,96 +7,167 @@ const defaultItemDisplayStyle = Array.from(
   document.styleSheets[1].cssRules
 ).find((e) => e.selectorText == '.items li').style.display;
 
+/* Load all of the items in local storage to the item list in the DOM; happens on page load
+ */
 function displayItems() {
   const itemsFromStorage = getItemsFromStorage();
   itemsFromStorage.forEach((item) => addItemToDOM(item));
 }
+
+/* The user has triggered a submit event from the add form. This will either add a brand
+new item to the list or increase the quantity of an item of that name already in the list.
+If an item is added, it must be added to the DOM and to storage. Otherwise, the DOM should
+be updated. Either way, clear the form afterwards.
+*/
 function onAddItemSubmit(e) {
   e.preventDefault();
 
-  const newItem = itemInput.value;
+  const newItemText = itemInput.value;
 
   // Validate Input
-  if (newItem === '') {
+  if (newItemText === '') {
     alert('Please add an item');
     return;
   }
 
-  addItemToDOM(newItem);
-  addItemToStorage(newItem);
+  const itemToAdd = addItemToStorage(newItemText);
+  if (itemToAdd !== null) {
+    addItemToDOM(itemToAdd);
+  }
+
   checkUI();
 
   itemInput.value = '';
 }
+
+/* Add a brand new item to the DOM, either because the user has added a new one or
+because the DOM is loaded for the first time and all of the items are retrieved from
+local storage. 
+
+Parameters: item is an object containing a name (string) and a quantity (integer).
+*/
 function addItemToDOM(item) {
   // Create list item
   const li = document.createElement('li');
-  li.appendChild(document.createTextNode(item));
+  li.appendChild(document.createTextNode(item.name));
 
-  const button = createButton('remove-item btn-link text-red');
+  li.appendChild(createItemQuantityBox(item.quantity, 'quantity'));
+  li.appendChild(quantityInput); //  createTextNode(` (${item.quantity})`));
+
+  const button = createRemoveButton('remove-item btn-link text-red');
   li.appendChild(button);
 
   // add the list item to the unordered list
   itemList.appendChild(li);
 }
 
-function createButton(classes) {
+/* In local storage, change the quantity of an item in the Shopping List to a new amount.
+
+Parameters: 
+  itemText (string) is the text of the item whose quantity is changing
+  amount (integer) is the new amount to be stored for that item
+*/
+function changeQuantityInStorageOf(itemText, amount) {
+  const itemsFromStorage = getItemsFromStorage();
+  const itemIndex = itemsFromStorage.findIndex((each) => each.name == itemText);
+  let itemToChange = itemsFromStorage[itemIndex];
+  itemToChange.quantity = amount;
+  itemsFromStorage[itemIndex] = itemToChange;
+  localStorage.setItem('items', JSON.stringify(itemsFromStorage));
+}
+
+function changeQuantityInDOMOf(itemText, amount) {
+  listItem = Array.from(itemList.children).find(
+    (each) => each.textContent == itemText
+  );
+  quantityBox = listItem.querySelector('input');
+  quantityBox.value = amount;
+}
+/* The user has attempted to add an item to the list. Check to see if the item is already
+in the list. If so, ask if they want to increase the quantity of that item. If they say
+okay to that, increase the quantity by one, otherwise, assume they made a mistake and
+don't want to add the item at all.
+If the item is not already in the list, simply add it.
+Either way, update the list and/or quantity in local storage
+Parameters: itemText (string) - the item to be added to the Shopping List
+Return: either the item added (if it's a new item) or null (if it's an increase or cancel)
+ */
+function addItemToStorage(itemText) {
+  const itemsFromStorage = getItemsFromStorage();
+  const itemIndex = itemsFromStorage.findIndex((each) => each.name == itemText);
+
+  if (itemIndex > -1) {
+    // item exists; check if increase is desired
+    if (
+      confirm(
+        `You already have ${itemText} in the list. Do you want to increase the quantity?`
+      )
+    ) {
+      // Yes, increase
+      const itemToIncrease = itemsFromStorage[itemIndex];
+      let currentAmount = itemToIncrease.quantity;
+      currentAmount++;
+
+      changeQuantityInStorageOf(itemText, currentAmount);
+      changeQuantityInDOMOf(itemText, currentAmount);
+    }
+    return null;
+  }
+  // Add new item to array
+  const itemToAdd = {
+    name: itemText,
+    quantity: 1,
+  };
+  itemsFromStorage.push(itemToAdd);
+
+  // Convert to JSON string and set to local storage
+  localStorage.setItem('items', JSON.stringify(itemsFromStorage));
+  return itemToAdd;
+}
+
+/* Create and return the part of the shopping list item that displays the quantity for that 
+item.
+Parameters: 
+  quantity (integer) - the quantity of that item to display
+  classes (string) - space-separated css classes
+
+Return: the input element that displays the quantity
+*/
+function createItemQuantityBox(quantity, classes) {
+  quantityInput = document.createElement('input');
+  quantityInput.setAttribute('type', 'number');
+  quantityInput.setAttribute('class', classes);
+  quantityInput.value = quantity;
+  return quantityInput;
+}
+
+/* Create and return the part of the shopping list item that is the remove button for 
+removing that item from the shopping list
+Parameters: classes (string) - space-separated css classes
+Return: the newly-created remove button element
+ */
+function createRemoveButton(classes) {
   const button = document.createElement('button');
   button.className = classes;
-  const icon = createIcon('fa-solid fa-xmark');
+  const icon = createRemoveIcon('fa-solid fa-xmark');
   button.appendChild(icon);
   return button;
 }
 
-function createIcon(classes) {
+/* Create and return the part of the remove button that shows the 'X' icon.
+Parameters: classes (string) - space-separated list of css classes
+Return: the newly-created icon element
+*/
+function createRemoveIcon(classes) {
   const icon = document.createElement('i');
   icon.className = classes;
   return icon;
 }
 
-function addItemToStorage(item) {
-  const itemsFromStorage = getItemsFromStorage();
-  const itemIndex = itemsFromStorage.indexOf(item);
-
-  const existingItems = itemsFromStorage.filter(
-    (each) => each.indexOf('(') > -1 && each.slice(0, each.indexOf('(')) == item
-  );
-
-  console.log(existingItems);
-
-  if (existingItems.length > 0) {
-    if (
-      confirm(
-        'You already have this item. Do you want to increase the quantity?'
-      )
-    ) {
-      let itemToIncrease = itemsFromStorage[itemIndex];
-      let currentAmount = 1;
-      openParenIndex = itemToIncrease.indexOf('(');
-      if (openParenIndex === -1) {
-        // Don't have parentheses yet
-        openParenIndex += itemToIncrease.length;
-      } else {
-        closeParenIndex = itemToIncrease.indexOf(')');
-        currentAmount = Number(
-          itemToIncrease.slice(openParenIndex, closeParenIndex)
-        );
-      }
-      console.log(currentAmount);
-      currentAmount++;
-      itemToIncrease =
-        itemToIncrease.slice(0, openParenIndex + 1) + `(${currentAmount})`;
-      itemsFromStorage[itemIndex] = itemToIncrease;
-    }
-  } else {
-    // Add new item to array
-    itemsFromStorage.push(item);
-  }
-
-  // Convert to JSON string and set to local storage
-  localStorage.setItem('items', JSON.stringify(itemsFromStorage));
-}
-
+/* Retrieve and return the objects from local storage that should appear in the 
+shopping list. Each item consists of a name and a quantity.
+Return: the Array of items from local storage, where each item has a name and quantity
+*/
 function getItemsFromStorage() {
   let itemsFromStorage;
 
@@ -109,42 +180,67 @@ function getItemsFromStorage() {
   return itemsFromStorage;
 }
 
-function removeItemFromDOM(e) {
-  e.target.parentElement.parentElement.remove();
+/* Remove from the DOM the given listItem from the list of shopping list items.
+Parameters: listItem - the li to be removed from the DOM.
+*/
+function removeItemFromDOM(listItem) {
+  listItem.remove();
 }
 
-function removeItemFromStorage(e) {
+/* Remove from local storage the given listItem from the list of shopping list items.
+  Parameters: listItem - the li whose textContent is to be removed from local storage.
+ */
+function removeItemFromStorage(listItem) {
   const itemsFromStorage = getItemsFromStorage();
-  const listItem = e.target.parentElement.parentElement;
-  const unorderedList = listItem.parentElement;
-  const index = Array.from(unorderedList.children).indexOf(listItem);
   const textToRemove = listItem.textContent;
-
+  const index = itemsFromStorage.findIndex(
+    (each) => each.name == listItem.textContent
+  );
   console.log(textToRemove);
-  // const index = itemsFromStorage.indexOf(textToRemove);
   if (index > -1) {
+    // found
     itemsFromStorage.splice(index, 1);
     localStorage.setItem('items', JSON.stringify(itemsFromStorage));
   }
 }
-
-function removeItem(e) {
-  // console.log(e.target.parentElement);
-  if (e.target.parentElement.classList.contains('remove-item')) {
-    // console.log(e.target.parentElement.classList);
-    // console.log(e.target.parentElement.parentElement);
-    if (confirm('Are you sure?')) {
-      removeItemFromStorage(e);
-      removeItemFromDOM(e);
-    }
-    checkUI();
+function confirmToRemove(listItem) {
+  const itemText = listItem.textContent;
+  if (confirm(`Are you sure you want to remove ${itemText} from the list?`)) {
+    removeItemFromStorage(listItem);
+    removeItemFromDOM(listItem);
+    return true;
   }
+  return false;
+}
+/* The user has clicked somewhere in the list item. This could be either in the remove icon
+or in the quantity box. Either remove the item from the DOM and from storage, or change
+its quantity in the DOM and in storage, accordingly.
+Parameter: e - the event that triggered this method
+*/
+function modifyItem(e) {
+  if (e.target.parentElement.classList.contains('remove-item')) {
+    const listItem = e.target.parentElement.parentElement;
+    confirmToRemove(listItem);
+  } else if (e.target.classList.contains('quantity')) {
+    const listItem = e.target.parentElement;
+    const itemText = listItem.textContent;
+    const quantity = e.target.value;
+    if (quantity == 0) {
+      if (!confirmToRemove(listItem)) {
+        e.target.value = 1;
+      }
+    } else {
+      changeQuantityInStorageOf(itemText, e.target.value);
+    }
+  }
+  checkUI();
 }
 
 function clearItems() {
   while (itemList.firstChild) {
     itemList.removeChild(itemList.firstChild);
   }
+  localStorage.setItem('items', JSON.stringify([]));
   checkUI();
 }
 
@@ -177,7 +273,9 @@ function filterItemsTraversy(e) {
 }
 
 function checkUI() {
-  const items = itemList.querySelectorAll('li');
+  const items = itemList.children; //querySelectorAll('li');
+  console.log(itemList, items);
+  console.log(items.length);
   if (items.length === 0) {
     clearBtn.style.display = 'none';
     itemFilter.style.display = 'none';
@@ -188,7 +286,7 @@ function checkUI() {
 }
 // Event Listeners
 itemForm.addEventListener('submit', onAddItemSubmit);
-itemList.addEventListener('click', removeItem);
+itemList.addEventListener('click', modifyItem);
 clearBtn.addEventListener('click', clearItems);
 itemFilter.addEventListener('input', filterItems);
 document.addEventListener('DOMContentLoaded', displayItems);
